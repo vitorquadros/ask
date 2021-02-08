b683f3b5cef4877d60eeea3e0dc1ab92a728b7bd
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-08 02:13:34 -0300

    initial form

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
 app.listen(3333, () => {
   console.log("Servidor iniciado.");
 });
