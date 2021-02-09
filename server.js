20fff20e9b41f5d0d0ee5c2f76b57d1a260285df
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-09 05:04:55 -0300

    database connection

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+// Database
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("conexão com o db feita.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
