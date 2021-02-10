28779d58b3493b61e1a2e3fe6213e5744cf2346e
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-09 22:28:04 -0300

    listing questions

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Pergunta.findAll({ raw: true, order: [["id", "DESC"]] }).then((perguntas) => {
+    res.render("index", {
+      perguntas: perguntas,
+    });
+  });
 });
 
 app.get("/perguntar", (req, res) => {
